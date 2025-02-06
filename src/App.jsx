0f66f61bd9d84b817cd04bf6df7a992d5263e796
vcovelli <182012017+vcovelli@users.noreply.github.com>
2025-02-06 15:56:31 -0500

    Updated Navbar for mobile compatibility.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "./components/Navbar";
+import Navbar from "./components/Navbar/Navbar";
 import AboutMe from "./components/AboutMe";
 import Experience from "./components/Experience/Experience";
 import Skills from "./components/Skills/Skills";
